0b8aed962a59fc810da2788d9eb76935da3b3000
MarcoLIU27 <lhy1421381989@gmail.com>
2023-03-31 10:27:55 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var cors = require("cors");
-var fs = require('fs');
-var path = require('path');
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -31,12 +29,6 @@ app.use("/users", usersRouter);
 app.use("/testAPI", testAPIRouter);
 app.use("/testDB", testDBRouter);
 app.use('/api/people', peopleRouter);
-
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
   
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -52,11 +44,6 @@ app.use(function(err, req, res, next) {
     // render the error page
     res.status(err.status || 500);
     res.render("error");
-});
-
-const port = process.env.PORT || 9000;
-app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
 });
 
 module.exports = app;
